5e78b2f5fb2f9746140767ebe9dd751091db2e7e
HD650 <zzhang63@ncsu.edu>
2018-05-17 17:02:28 -0400

    small fix

--- database/object/tile.py
+++ database/object/tile.py
@@ -4,12 +4,14 @@ tiles = \
     {
         "stone_ground":
             {
+                "name": "stone ground",
                 "key_word": [material.materials["stone"]],
                 "texture": "tile/stone_ground.png",
                 "canWalk": True,
             },
         "grass_ground":
             {
+                "name": "grass ground",
                 "key_word": [material.materials["grass"]],
                 "texture": "tile/grass_ground.png",
                 "canWalk": True
--- rogue.py
+++ rogue.py
@@ -1,4 +1,6 @@
 import engine
 
 if __name__ == '__main__':
+    engine.g_engine.get_ready()
     engine.g_engine.run()
+
